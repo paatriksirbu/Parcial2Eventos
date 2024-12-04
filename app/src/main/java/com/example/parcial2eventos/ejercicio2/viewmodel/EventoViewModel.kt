bfef8bac56c793acea36f5fc4972a8de97107bde
Patrik <psirb@myuax.com>
2024-12-04 20:16:13 +0100

    EventoViewModel

--- app/src/main/java/com/example/parcial2eventos/ejercicio2/viewmodel/EventoViewModel.kt
+++ app/src/main/java/com/example/parcial2eventos/ejercicio2/viewmodel/EventoViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.parcial2eventos.ejercicio2.viewmodel
 
-class EventoViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.parcial2eventos.ejercicio2.data.model.Evento
+import com.example.parcial2eventos.ejercicio2.data.repository.EventoRepository
+import com.google.android.gms.tasks.Task
+
+class EventoViewModel(private val repository: EventoRepository) : ViewModel() {
+
+    fun obtenerEventos(): LiveData<List<Evento>> {
+        return repository.getEventos()
+    }
+
+    fun guardarEvento(evento: Evento): Task<Void> {
+        return repository.saveEvento(evento)
+    }
 }
