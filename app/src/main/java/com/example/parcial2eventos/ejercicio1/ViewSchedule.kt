7a0a50c9194d64da0a134545fd2bdd1b090efe05
Patrik <psirb@myuax.com>
2024-12-04 18:44:10 +0100

    Inicializacion de las variables

--- app/src/main/java/com/example/parcial2eventos/ejercicio1/ViewSchedule.kt
+++ app/src/main/java/com/example/parcial2eventos/ejercicio1/ViewSchedule.kt
@@ -3,6 +3,7 @@ package com.example.parcial2eventos.ejercicio1
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parcial2eventos.R
 import com.example.parcial2eventos.ejercicio1.model.Clase
@@ -16,5 +17,24 @@ class ViewSchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_schedule)
+
+        // Inicializar vistas
+        recyclerView = findViewById(R.id.recyclerViewSchedule)
+
+        // Configurar RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = ScheduleAdapter(classesList)
+        recyclerView.adapter = adapter
+
+        // Configurar botones de dias.
+        dayButtons = listOf(
+            findViewById(R.id.btnMonday),
+            findViewById(R.id.btnTuesday),
+            findViewById(R.id.btnWednesday),
+            findViewById(R.id.btnThursday),
+            findViewById(R.id.btnFriday)
+        )
+
+
     }
 }
